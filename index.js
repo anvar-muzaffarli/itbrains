80099aa1f7fe0a165fd9d4a130ff01918f849097
anvar-muzaffarli <prof.anvar2013@gmail.com>
2021-06-01 22:36:14 +0400

    Mehemmed beyin xahishi ile ilk proyektimizin ilk commitinin yaradilmasi

---
+++ index.js
@@ -0,0 +1,6 @@
+const ITBrainsTelebeleriniSertifikatlaTeminEt = (name,surname) => {
+    console.log(`IT Brains telebesi ${name} ${surname} sertifikatla temin olunur.`);
+}
+
+ITBrainsTelebeleriniSertifikatlaTeminEt("Arzu", "Bedelzade")
+
